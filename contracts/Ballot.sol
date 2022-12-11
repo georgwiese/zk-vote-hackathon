603d999043e26870557ce7511c2f0c1b524ada53
valentin-pinkau <valentin.pinkau@scalableminds.com>
2022-12-11 13:22:12 +0100

    pass proof and hardcode input in contract to debug

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -59,31 +59,56 @@ contract Ballot {
         commits[commit] = true;
         commitList.push(commit);
     }
+    
 
     function revealVote(bool _vote, bytes32 serialNumber, bytes32[10] memory commitsForProof, Verifier.Proof memory proof) public {
         // validate proof
         // call sokrates
         uint[85] memory proof_inputs;
-        uint j = 0;
-        if (_vote) {
-            proof_inputs[j] = 1;
-        } else {
-            proof_inputs[j] = 0;
+        //uint j = 0;
+        //if (_vote) {
+        //    proof_inputs[j] = 1;
+        //} else {
+        //    proof_inputs[j] = 0;
+        //}
+        //j += 1;
+        //for(uint i = 3; i >= 0; --i) {
+        //    proof_inputs[j] = uint256(serialNumber >> (i * 32)) & 0xffffffff ;
+        //    j += 1;
+        //    if(i == 0) {
+        //        break;
+        //    }
+        //}
+        //for(uint c_index = 0; c_index < 10; c_index++) {
+        //    bytes32 commit = commitsForProof[c_index];
+        //    for(uint i = 7; i >= 0; i--) {
+        //        proof_inputs[j] = uint256(commit >> (i * 32)) & 0xffffffff ;
+        //        j += 1;
+        //        if(i == 0) {
+        //            break;
+        //        }
+        //    }
+        //}
+
+        proof_inputs[0] = 0x0000000000000000000000000000000000000000000000000000000000000001;
+        proof_inputs[1] = 0x00000000000000000000000000000000000000000000000000000000081c316d;
+        proof_inputs[2] = 0x0000000000000000000000000000000000000000000000000000000063f8fc53;
+        proof_inputs[3] = 0x00000000000000000000000000000000000000000000000000000000ed350298;
+        proof_inputs[4] = 0x000000000000000000000000000000000000000000000000000000005b7b2544;
+        proof_inputs[5] = 0x0000000000000000000000000000000000000000000000000000000072364d22;
+        proof_inputs[6] = 0x00000000000000000000000000000000000000000000000000000000777dc5c5;
+        proof_inputs[7] = 0x00000000000000000000000000000000000000000000000000000000b4473fd2;
+        proof_inputs[8] = 0x00000000000000000000000000000000000000000000000000000000807d56be;
+        proof_inputs[9] = 0x0000000000000000000000000000000000000000000000000000000087d5c06b;
+        proof_inputs[10] = 0x00000000000000000000000000000000000000000000000000000000c6b1fcdc;
+        proof_inputs[11] = 0x000000000000000000000000000000000000000000000000000000002e44b540;
+        proof_inputs[12] = 0x00000000000000000000000000000000000000000000000000000000412d15c4;
+        for(uint i = 13; i < 85; ++i) {
+            proof_inputs[i] = 0x0000000000000000000000000000000000000000000000000000000000000000;
         }
-        j += 1;
-        for(uint i = 3; i >= 0; i--) {
-            proof_inputs[j] = uint256(serialNumber >> (i * 32)) & 0xffffffff ;
-            j += 1;
-        }
-        for(uint c_index = 0; c_index < 10; c_index++) {
-            bytes32 commit = commitsForProof[c_index];
-            for(uint i = 7; i >= 0; i--) {
-                proof_inputs[j] = uint256(commit >> (i * 32)) & 0xffffffff ;
-                j += 1;
-            }
-        }
+
         Verifier v = Verifier(verifierContractAddress);
-        require(v.testVerifyTx(proof, proof_inputs));
+        require(v.verifyTx(proof, proof_inputs));
 
         // is commitsForProof subset of commits
         for (uint i = 0; i < commitsForProof.length; ++i) {
